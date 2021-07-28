debf7a9ad73f065187e5fca5c0e00f4914301e36
Yuu-stack <hi202rotaka328.02@gmail.com>
2021-07-29 00:46:47 +0900

    [add] 取得ランキングをCSV,JSONへ出力
    1. get_info()(取得データを解析,辞書型に整形する)メソッドの追加
    2. write(info)(CSV,JSONへの書き出しをする)メソッドの追加
    3. data_view()(出力したJSONデータを表示する)メソッドの追加

--- amazon_ranking.py
+++ amazon_ranking.py
@@ -3,6 +3,10 @@ import requests
 from requests.packages.urllib3.util import Retry
 from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
+import time
+import datetime
+import pandas as pd
+import json
 
 
 # Amazon ランキング　
@@ -25,26 +29,19 @@ _BASE_URL = "https://www.amazon.co.jp/"
 _CATEGORY = 'digital-text'
 _BROWSE_NODE_ID = '2293143051'
 _DEFAULT_BEAUTIFULSOUP_PARSER = "html.parser"
-_DEFAULT_USER_AGENT = 'Mozilla/5.0 (Linux; Android 7.0; \
-SM-A520F Build/NRD90M; wv) AppleWebKit/537.36 \
-(KHTML, like Gecko) Version/4.0 \
-Chrome/65.0.3325.109 Mobile Safari/537.36'
-_CHROME_DESKTOP_USER_AGENT = 'Mozilla/5.0 (Macintosh; \
-Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) \
-Chrome/67.0.3396.79 Safari/537.36'
-
-_USER_AGENT_LIST = [
-    _DEFAULT_USER_AGENT,
-    _CHROME_DESKTOP_USER_AGENT,
-]
-
+_TODAY = datetime.datetime.now().strftime('%Y.%m.%d-%H-%M')
 
 info = []
 
+# 型と中身を表示させる関数
+def print_data(data):
+    print(type(data))
+    print(data)
 
 def ama(load_url):
 	# ステータスコードでのリトライ
 	# 参考: https://qiita.com/azumagoro/items/3402facf0bcfecea0f06
+	time.sleep(2)
 	session = requests.Session()
 	retries = Retry(total=5,  # リトライ回数
                 backoff_factor=1,  # sleep時間
@@ -65,9 +62,42 @@ def ama(load_url):
 		sys.exit()
 
 	else:
-		soup = BeautifulSoup(html.content, _DEFAULT_BEAUTIFULSOUP_PARSER)
-		return soup
+		print(html.status_code)
 
+	print(html.text[:500])
+	soup = BeautifulSoup(html.content, _DEFAULT_BEAUTIFULSOUP_PARSER)
+	return soup
+
+
+def get_info(ele):
+	RANK = ele.find("span", class_="zg-badge-text").text
+	TITLE = ele.find("div", class_="p13n-sc-truncate").text.strip()
+	AUTHOR = ele.find("div", class_="a-row a-size-small").text
+	PRICE = ele.find("span", class_="p13n-sc-price").text
+	PICTURE_tag = ele.find("div", class_="a-section a-spacing-small")
+	PICTURE_URL = PICTURE_tag.find("img").get("src")
+	MAIN_PAGE_tag = ele.find("span", class_="aok-inline-block zg-item")
+	MAIN_PAGE_URL = MAIN_PAGE_tag.find("a", class_="a-link-normal").get("href")
+
+	# 作品ページから無駄な文字列の削除
+	idx = MAIN_PAGE_URL.find(_TARGET_WORD_1)  # 半角空白文字のインデックスを検索
+	# 検索文字列の先頭文字の場所が idxに格納される
+	#retext = MAIN_PAGE_URL[idx+3:idx+13] #dp/の3文字分をずらしてURLから10文字分スライス
+	retext = MAIN_PAGE_URL[idx+3:] #dp/の3文字分をずらし
+	idx2 = retext.find(_TARGET_WORD_2)
+	ASIN = retext[:idx2]
+	MAIN = _BASE_URL + _TARGET_WORD_1 + ASIN
+
+	info = {
+	"ranking": RANK,
+	"title": TITLE,
+	"author": AUTHOR,
+	"price": PRICE,
+	"img": PICTURE_URL,
+	"asin": ASIN,
+	"url": MAIN
+	}
+	return info
 
 
 def get_ViewInfo(ele):
@@ -83,7 +113,8 @@ def get_ViewInfo(ele):
 	# 作品ページから無駄な文字列の削除
 	idx = MAIN_PAGE_URL.find(_TARGET_WORD_1)  # 半角空白文字のインデックスを検索
 	# 検索文字列の先頭文字の場所が idxに格納される
-	retext = MAIN_PAGE_URL[idx+3:]
+	#retext = MAIN_PAGE_URL[idx+3:idx+13] #dp/の3文字分をずらしてURLから10文字分スライス
+	retext = MAIN_PAGE_URL[idx+3:] #dp/の3文字分をずらし
 	idx2 = retext.find(_TARGET_WORD_2)
 	ASIN = retext[:idx2]
 	MAIN = _BASE_URL + _TARGET_WORD_1 + ASIN
@@ -100,25 +131,52 @@ def get_ViewInfo(ele):
 	print("MainPage: "+ MAIN)
 	print("= = = = = = = = = = =  = = = = = = = =  = = = = = = =  = = = = =  = = = =  =")
 
+def write(info):
+	time.sleep(2)
+	# utf-8で書き込み
+	with open('Amazon' + str(_TODAY) + '.json', 'w', encoding='utf-8_sig') as fp:
+		# 辞書(info)をインデントをつけてアスキー文字列ではない形で保存
+	    json.dump(info, fp, indent=7, ensure_ascii=False )
+	# 書き込みオブジェクトを閉じる
+	fp.close()
+
+	#pandasを使用してCSV形式で出力
+	df = pd.DataFrame(info)
+	df.to_csv('Amazon' + str(_TODAY) + '.csv',encoding='utf-8_sig')  
+
+
+	
+def data_view():
+	#取得したJsonデータを表示
+	with open('Amazon' + str(_TODAY) + '.json', 'r', encoding='utf-8_sig') as fp:
+		data = json.load(fp)
+
+	# 書き込みオブジェクトを閉じる
+	fp.close()
+	print_data(data)
+
 
 def main():
-	load_url = _BASE_URL + 'gp/bestsellers/' +  _CATEGORY + '/' + _BROWSE_NODE_ID + '/' + '?pg=' + "1"
-	print("++++++++++++++++++++++++++++++++++++++++++++++++")
-	print(" ")
-	print(load_url)
-	print(" ")
-	print("++++++++++++++++++++++++++++++++++++++++++++++++")
-	soup = ama(load_url)
+	for n in range(1,3):
+		load_url = _BASE_URL + 'gp/bestsellers/' +  _CATEGORY + '/' + _BROWSE_NODE_ID + '/' + '?pg=' + str(n)
+		print("++++++++++++++++++++++++++++++++++++++++++++++++")
+		print(" ")
+		print(load_url)
+		print(" ")
+		print("++++++++++++++++++++++++++++++++++++++++++++++++")
+		soup = ama(load_url)
+		# すべてのliタグを検索して、その文字列を表示する
+		for ele in soup.find_all("li", class_="zg-item-immersion"):
+			get_ViewInfo(ele)
+			info.append(get_info(ele))
 
+		write(info)
+	
 
-
-	# すべてのliタグを検索して、その文字列を表示する
-	for ele in soup.find_all("li", class_="zg-item-immersion"):
-		get_ViewInfo(ele)
-  
 
 if __name__ == '__main__':
     main()
+    data_view()
 
 
 
